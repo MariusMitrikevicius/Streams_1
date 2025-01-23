a1a253f56c89badd4c26541aa893dabb9121c26f
MariusMitrikevicius <zandenis@gmail.com>
2025-01-23 20:11:46 +0200

    Užduotis nr. 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Main {
         public static void main(String[] args) {
-            List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "grapes");
 
+
+            List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "grapes");
             System.out.println("Words list: " + words);
             System.out.println("Žodžiai ilgesni nei 5r.:");
             for (String word : words) {
@@ -18,8 +19,32 @@ public class Main {
             List<String> longWords = words.stream()
                     .filter(word -> word.length() > 5)
                     .toList();
-
             System.out.println("Žodžiai ilgesni nei 5r. " + longWords);
+            System.out.println();
+
+
+
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+            int sum = 0;
+            for (Integer number : numbers) {
+                sum += number;
+            }
+            System.out.println("Skaičių suma: " + sum);
+            System.out.println();
+            int sum1 = numbers.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+            System.out.println("The sum of all numbers: " + sum1);
+
+
+
+
+
+
+
+
+
+
 
         }
 }
