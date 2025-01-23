3682a5f0a12b808ef16fd293b4cdd9dbfd00a77e
MariusMitrikevicius <zandenis@gmail.com>
2025-01-23 19:27:46 +0200

    Užduotis nr. 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        public static void main(String[] args) {
+            List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "grapes");
+
+            System.out.println("Words list: " + words);
+            System.out.println("Žodžiai igesni nei 5r.:");
+            for (String word : words) {
+                if (word.length() > 5) {
+                    System.out.println(word);
+                }
+            }
+            System.out.println();
+            List<String> longWords = words.stream()
+                    .filter(word -> word.length() > 5)
+                    .toList();
+
+            System.out.println("Žodžiai igesni nei 5r. " + longWords);
+
+        }
 }
